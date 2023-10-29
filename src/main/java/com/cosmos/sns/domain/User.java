4e6e8745bcc0f5f3cbbe427b43e02740da2c89aa
ness727 <gnsdl727@naver.com>
2023-10-29 14:50:47 +0900

    유저 추가시 검증 코드 추가

--- src/main/java/com/cosmos/sns/domain/User.java
+++ src/main/java/com/cosmos/sns/domain/User.java
@@ -1,6 +1,7 @@
 package com.cosmos.sns.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -17,19 +18,33 @@ public class User extends CreatedDate {
     @Column(name = "id", updatable = false)
     private Long id;
 
-    @Column(name = "email", nullable = false, length = 20)
+    @Column(name = "email", nullable = false)
+    @NotBlank(message = "이메일을 입력해주세요")
+    @Size(min = 1, max = 50, message = "50자 이내로 입력해주세요")
+    @Pattern(regexp = "[\\w+.-]+@[\\w+-]+.[\\w+.-]+", message = "이메일 형식으로 입력해 주세요")  // 영문자, 숫자, 밑줄만 가능
     private String email;
 
-    @Column(name = "pwd", nullable = false, length = 20)
+    @Column(name = "pwd", nullable = false)
+    @NotBlank(message = "비밀번호를 입력해주세요")
+    @Size(min = 1, max = 20, message = "20자 이내로 입력해주세요")
+    @Pattern(regexp = "[\\w0-9!@#+=%^*-]+", message = "사용할 수 없는 문자가 포함되어 있습니다.")
+        // 영문자, 일부 특수기호, 숫자, 밑줄만 가능
     private String pwd;
 
-    @Column(name = "user_name", nullable = false, length = 50)
+    @Column(name = "user_name", nullable = false)
+    @NotBlank(message = "이름를 입력해주세요")
+    @Size(min = 1, max = 30, message = "30자 이내로 입력해주세요")
+    @Pattern(regexp = "[가-힣a-zA-Z]+", message = "사용할 수 없는 문자가 포함되어 있습니다.")  // 한글, 영문 이름만 가능
     private String userName;
 
-    @Column(name = "nickname", nullable = false, length = 20)
+    @Column(name = "nickname", nullable = false)
+    @NotBlank(message = "별명을 입력해주세요")
+    @Size(min = 1, max = 20, message = "20자 이내로 입력해주세요")
     private String nickname;
 
-    @Column(name = "phone", nullable = false, length = 13)
+    @Column(name = "phone", nullable = false)
+    @NotBlank(message = "핸드폰 번호를 입력해주세요")
+    @Size(min = 1, max = 13)
     private String phone;
 
     @Column(name = "profile_image")
